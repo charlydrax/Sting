ea0f43cfc261de283b1cb21cb87107e038b8b995
Yezir971 <james_ahmedaly@yahoo.com>
2024-09-13 17:22:57 +0200

    set-up

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Error from "./pages/Error/Error"
+
 export default function App() {
-  
+    return(
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Error />} />
+        </Routes>
+    )
 };
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return(
+        <p>ceci est la page home </p>
+    )
+}
