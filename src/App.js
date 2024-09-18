99fb1a2fa83ce0aa831db25c9e52cf8b9e09eb65
Yezir971 <james_ahmedaly@yahoo.com>
2024-09-18 09:28:12 +0200

    Nav bar, page 404

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Error from "./pages/Error/Error"
+import "../src/style/css/app.css"
+import Nav from "./components/nav/Nav"
+
 export default function App() {
-  
+    return(
+        <>
+        
+        <Nav />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Error />} />
+        </Routes>
+        </>
+    )
 };
