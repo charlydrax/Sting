58e464794d1191cb83417a86f5cbb1131f53dbd1
Yezir971 <james_ahmedaly@yahoo.com>
2024-10-24 14:46:59 +0200

    responsive page jeux J

--- src/pages/Test/James/James.jsx
+++ src/pages/Test/James/James.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import InfoJeux from "../../../components/InfoJeux/InfoJeux"
+import Footer from "../../../components/footer/Footer";
 
 function James (){
     const { id } = useParams();
